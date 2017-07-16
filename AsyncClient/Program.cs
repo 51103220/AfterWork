e1b182e435cf557b0d2f250f925355fdb1ed2eab
51103220 <rewandedo1009@gmail.com>
2017-07-16 22:00:21 +0700

    Add project files.

---
+++ AsyncClient/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncClient
+{
+    class Server
+    {
+        private const int PORT = 11111;
+        private const int BUFFER_SIZE = 5000;
+        private const int NUM_OF_REQUESTS = 1000;
+
+        static async Task<string> Call(int index)
+        {
+            try
+            {
+                using (TcpClient client = new TcpClient())
+                {
+                    // 1. connect
+                    IPAddress ipAddress = Dns.GetHostEntry("localhost").AddressList[1];
+                    await client.ConnectAsync(ipAddress, PORT);
+                    Stream stream = client.GetStream();
+                    Console.WriteLine($"Request {index} connected to server.");
+
+                    // 2. send
+                    string str = $"Request {index}";
+
+                    byte[] data = Encoding.ASCII.GetBytes(str);
+
+                    await stream.WriteAsync(data, 0, data.Length);
+
+                    // 3. receive
+                    byte[] received = new byte[5000];
+                    await stream.ReadAsync(received, 0, received.Length);
+                    var response = Encoding.ASCII.GetString(data);
+                    Console.WriteLine($"\t Request {index} has reponse: {response}");
+                    return response;
+                }
+                
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex);
+            }
+
+            return string.Empty;
+
+        }
+        static Task<string[]> MakeRequestsAsync()
+        {
+            List<string> responses = new List<string>();
+            int[] indexes = new int[NUM_OF_REQUESTS];
+            for (var i = 0; i < NUM_OF_REQUESTS; i++)
+            {
+                indexes[i] = i;
+            }
+            return Task.WhenAll(indexes.Select(async i => await Call(i)).ToList());
+            
+        }
+
+        static void Main(string[] args)
+        {
+            string[] responses = MakeRequestsAsync().Result;
+
+            var nbOfSuccessfulResponses = responses.Count(x => x != string.Empty);
+            Console.WriteLine($"{nbOfSuccessfulResponses}/{NUM_OF_REQUESTS} requests is made successfully");
+
+            Console.WriteLine("Done Testing");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AsyncServer/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncServer
+{
+    class Server
+    {   
+        // maximum threads in thread pool 
+        private const int NUMBER_OF_THREADS = 4;
+        private const int PORT = 11111;
+        private const int BUFFER_SIZE = 5000;
+        static async void RunServerAsync()
+        {
+            IPAddress ipAddress = Dns.GetHostEntry("localhost").AddressList[1];
+            var listener = new TcpListener(ipAddress, PORT);
+           
+
+            listener.Start();
+            Console.WriteLine($"listening from {ipAddress.ToString()}:{PORT} ...");
+            try
+            {
+                while (true)
+                {
+                    Accept(await listener.AcceptTcpClientAsync());
+                }
+            }
+            finally { listener.Stop(); }
+        }
+
+        static async void Accept(TcpClient client)
+        {
+            await Task.Yield();
+            Console.WriteLine($"Connect established from client {((IPEndPoint)client.Client.RemoteEndPoint).Address.ToString()}");
+            try
+            {
+                using (client)
+                using (NetworkStream n = client.GetStream())
+                {
+                    byte[] data = new byte[5000];
+                    await n.ReadAsync(data, 0, data.Length);
+                      
+                    Console.WriteLine($"\t Start to respond to client {Encoding.ASCII.GetString(data)}...waiting for 15s");
+                    await Task.Delay(TimeSpan.FromSeconds(15));
+
+                    Array.Reverse(data); // Reverse the byte sequence
+                    await n.WriteAsync(data, 0, data.Length);
+                    Console.WriteLine("\t done responding to client");
+                }
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+        }
+
+        static void Main(string[] args)
+        {
+            ThreadPool.SetMaxThreads(NUMBER_OF_THREADS, NUMBER_OF_THREADS);
+            RunServerAsync();
+            Console.ReadKey();
+        }
+    }
+}
